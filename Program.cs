c8714ef99206901b7dd47486c5221ee07a9dee4f
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-09-13 12:13:35 -0500

    Cantidad de productos

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace ExpendedoraProyecto_G3_2022_I
     {
         static void Main()
         {
-            Expendedora miExpendedora = new Expendedora();
+            /*Expendedora miExpendedora = new Expendedora();
             Console.WriteLine("**************************");
             Console.WriteLine("Ingrese la marca de la expendedora que desea probar");
             string marca = Console.ReadLine();
-            Expendedora miExpendedoraPrueba = new Expendedora(marca);
+            Expendedora miExpendedoraPrueba = new Expendedora(marca);*/
             Console.WriteLine("**************************");
             Console.WriteLine("Ingrese la cantidad de productos de la expendedora que desea probar");
-            string cantProductos = Console.ReadLine();
+            ushort cantProductos =ushort.Parse(Console.ReadLine());
             Expendedora miExpendedoraPruebaDos = new Expendedora(cantProductos);
 
             Console.ReadLine();
